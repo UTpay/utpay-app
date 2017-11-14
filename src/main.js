458478368161683c5ab088e44abb23267d168307
m1nam1 <info@minami.xyz>
2017-11-15 00:30:27 +0900

    Change Onsen UI theme to dark

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'onsenui/css/onsenui.css'
-import 'onsenui/css/onsen-css-components.css'
+import 'onsenui/css/dark-onsen-css-components.css'
 
 import Vue from 'vue'
 import VueOnsen from 'vue-onsenui'
