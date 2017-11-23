870d54655259cac6f3d48b9b4b9e77b0579fe301
m1nam1 <info@minami.xyz>
2017-11-24 01:24:17 +0900

    Create Transaction page (model page)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Settings } from '../providers/providers';
 import { User } from '../providers/providers';
 import { Api } from '../providers/providers';
 import { MyApp } from './app.component';
+import { TransactionPage } from '../pages/transaction/transaction';
 
 // The translate loader needs to know where to load i18n files
 // in Ionic's static asset pipeline.
@@ -38,7 +39,8 @@ export function provideSettings(storage: Storage) {
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    TransactionPage
   ],
   imports: [
     BrowserModule,
@@ -55,7 +57,8 @@ export function provideSettings(storage: Storage) {
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    TransactionPage
   ],
   providers: [
     Api,
---
+++ src/pages/transaction/transaction.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+
+interface Transaction {
+  id: number;
+  user: any;
+  eth_account: any;
+  tx_hash: string;
+  from_address: string;
+  to_address: string;
+  amount: number;
+  gas: number;
+  gas_price: number;
+  value: number;
+  network_id: number;
+  is_active: boolean;
+  created_at: string;
+};
+
+@IonicPage()
+@Component({
+  selector: 'page-transaction',
+  templateUrl: 'transaction.html',
+})
+export class TransactionPage {
+  transaction: Transaction;
+
+  constructor(public navParams: NavParams, public viewCtrl: ViewController) {
+    this.transaction = navParams.get('transaction');
+    console.log(this.transaction);
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+}
