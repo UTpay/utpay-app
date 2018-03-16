ac80d07ffc5e5264bb0ca5ac7433e3eed0aade3e
m1nam1 <info@minami.xyz>
2018-03-17 02:56:28 +0900

    Create transactions page

---
+++ src/pages/transactions/transactions.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { IonicPage, ModalController } from 'ionic-angular';
+
+import { Api } from '../../providers/api/api';
+import { User } from '../../providers/user/user';
+import { TransactionPage } from '../transaction/transaction';
+
+interface Transaction {
+  id: number;
+  from_address: string;
+  to_address: string;
+  amount: number;
+  is_active: boolean;
+  created_at: string;
+};
+
+@IonicPage()
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html',
+})
+export class TransactionsPage {
+  pageTitle: string = '取引履歴';
+
+  transactions: Transaction[] = [];
+
+  constructor(
+    private modalCtrl: ModalController,
+    public api: Api,
+    public user: User
+  ) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TransactionsPage');
+    this.getTransactions();
+  }
+
+  getTransactions() {
+    console.log('getTransactions');
+    const headers = { 'Authorization': `Bearer ${this.user.userdata.token}` };
+    let req = this.api.get('transactions/?ordering=-id', null, headers).share();
+    req.subscribe(resp => {
+      console.log('transactions:', resp['results']);
+      this.transactions = resp['results'];
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return req;
+  }
+
+  presentDetailModal(transaction) {
+    const modal = this.modalCtrl.create(TransactionPage, { transaction });
+    modal.present();
+  }
+
+  doRefresh(refresher) {
+    this.getTransactions().subscribe(resp => refresher.complete());
+  }
+
+}
